8baa2200b252efb572aeaea5e72fc762de5e30e5
az0vtsev <a.az0vtsev@yandex.ru>
2022-10-26 03:24:21 +0300

    sprint 14 fix checkstyle

--- src/main/java/ru/practicum/shareit/exception/NotValidUserException.java
+++ src/main/java/ru/practicum/shareit/exception/NotValidUserException.java
@@ -1,6 +1,6 @@
 package ru.practicum.shareit.exception;
 
-public class NotValidUserException extends Exception{
+public class NotValidUserException extends Exception {
     public NotValidUserException(String message) {
         super(message);
     }
